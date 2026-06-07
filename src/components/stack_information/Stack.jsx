efb8480d8a57f5272b97836abaf2e501b33c5b89
Kairu <abandiakyle2112@gmail.com>
2026-06-07 13:09:04 +0800

    Feat: Scroll in techstack view restores its value up to the top when see more button is clicked

--- src/components/stack_information/Stack.jsx
+++ src/components/stack_information/Stack.jsx
@@ -1,40 +1,38 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useStack } from "../../context/stack";
 
-const CreateStack = (props) => {
+export const Stack = () => {
+  const [seeMore, setSeeMore] = useState(false);
   const { stacks } = useStack();
-  const { seeState } = props;
+  const stackRef = useRef(null);
   const listItems = stacks.map((items) => {
     return (
       <li>
-        <div className="bg-[#090e1a] border border-gray-800 flex flex-col items-center p-3 rounded-lg">
-          <items.icon className="h-10 w-10" />
-          <div>{items.title}</div>
+        <div className="bg-[#090e1a] border border-gray-800 flex flex-col items-center p-3 rounded-lg size-20">
+          <items.icon className="h-full w-full" />
+          <div className="text-white text-sm mt-2">{items.title}</div>
         </div>
       </li>
     );
   });
   return (
-    <ul
-      className={`flex flex-wrap transition-all duration-500 ${seeState ? "overflow-y-clip max-h-50" : "overflow-hidden max-h-24"} justify-center gap-3`}
-    >
-      {listItems}
-    </ul>
-  );
-};
-
-export const Stack = () => {
-  const [seeMore, setSeeMore] = useState(false);
-  return (
     <div className="flex flex-col items-center">
       <div className="font-semibold text-xs text-purple-500">TECH STACK</div>
       <div className="text-white text-xl">Technologies I use</div>
-      <div className="flex flex-col items-center">
-        <CreateStack seeState={seeMore} />
+      <div className="flex flex-col items-center mt-3">
+        <ul
+          ref={stackRef}
+          className={`flex flex-wrap transition-all duration-500 ${seeMore ? " sm:overflow-y-clip sm:max-h-220 overflow-y-scroll max-h-40 " : "overflow-hidden max-h-20"} justify-center gap-3`}
+        >
+          {listItems}
+        </ul>
         <div className="bg-transparent border text-white border-white rounded-full py-2 px-6 mt-4 hover:bg-white hover:text-black transition-colors duration-500">
           <button
-            className="cursor-pointer "
-            onClick={() => setSeeMore(!seeMore)}
+            className="cursor-pointer"
+            onClick={() => {
+              stackRef.current.scrollTo({ top: 0, behavior: "smooth" });
+              setSeeMore(!seeMore);
+            }}
           >
             {seeMore ? "See Less" : "See More"}
           </button>
