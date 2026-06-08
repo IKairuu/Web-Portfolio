a914705de3b0c55d36c7e4311ff75ce6de346deb
Kairu <abandiakyle2112@gmail.com>
2026-06-08 12:07:47 +0800

    Feat: Added project buttons with image, title and description

--- src/components/projects/Projects..jsx
+++ src/components/projects/Projects..jsx
@@ -1,10 +1,39 @@
+import { useProjects } from "../../context/projects";
+
 export const Projects = () => {
+  const { projects } = useProjects();
+  const projectList = projects.map((items) => {
+    const stacks = items.stack.map((tech) => {
+      return (
+        <li className="bg-[#141529] border border-gray-800 py-2 px-4 rounded-xl text-white text-xs">
+          {tech}
+        </li>
+      );
+    });
+    return (
+      <li className="h-70 w-70 flex flex-none flex-col rounded-lg border border-gray-800 cursor-pointer transition-all hover:-translate-y-5">
+        <div className="flex-1 rounded-t-lg">
+          <img src={items.image} className="rounded-t-lg w-full h-40" />
+        </div>
+        <div className="flex-1 h-full bg-[#090e1a] border border-gray-800 rounded-b-lg p-2">
+          <div className="font-bold text-white">{items.title}</div>
+          <div className="text-slate-500 text-xs overflow-y-auto h-20">
+            {items.description}
+          </div>
+          <ul className="flex mt-2 overflow-x-auto gap-2">{stacks}</ul>
+        </div>
+      </li>
+    );
+  });
   return (
-    <div className="flex flex-col items-center mt-3">
+    <div className="flex flex-col items-center sm:items-start mt-3">
       <div className="text-purple-500 font-semibold">PROJECTS</div>
       <div className="text-white text-lg font-bold sm:text-xl">
         SOME OF MY WORK
       </div>
+      <ul className="flex overflow-x-scroll max-w-full gap-4 mt-5 p-5">
+        {projectList}
+      </ul>
     </div>
   );
 };
---
+++ src/context/projects.js
@@ -0,0 +1,42 @@
+import { create } from "zustand";
+import hakbangApp from "../../src/assets/hakbangApp.png";
+import itsukiBot from "../../src/assets/itsukiBot.png";
+
+export const useProjects = create((set) => ({
+  projects: [
+    {
+      image: hakbangApp,
+      title: "Hakbang",
+      description:
+        "HAKBANG is an educational guidance platform designed to help Filipino senior high school students take the right hakbang toward making informed and strategic decisions as they prepare for higher education.",
+      stack: ["Flutter", "Dart", "Firebase", "Git", "Github"],
+      link: "",
+    },
+    {
+      image: itsukiBot,
+      title: "Discord Itsuki Bot",
+      description:
+        "Itsuki Nakano Discord Study Bot - Your personal study partner with the voice and attitude of Itsuki Nakano from The Quintessential Quintuplets! She’s flustered, stubborn, and surprisingly reliable. Whether you're reviewing, taking a quiz, or asking for help, she’s always ready with a tsundere comment to keep you motivated.",
+      stack: ["Python", "Discord.py"],
+      link: "",
+    },
+    {
+      image: hakbangApp,
+      title: "Hakbang Server",
+      description:
+        "Backend API Server for Hakbang Application with RestAPI Integrations managing requests and responses from API layers. A Three-Layer-Architecture designed to be scalable and manageable layers.",
+      stack: [
+        "Javascript",
+        "Node.js",
+        "Express.js",
+        "JWT",
+        "Postman",
+        "Prisma",
+        "Neon",
+        "PostgreSQL",
+        "Render",
+      ],
+      link: "",
+    },
+  ],
+}));
